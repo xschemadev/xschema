d9ef91a74edc513172f9f38436a7f4439b32061a
rmdevv <rm.swdev@gmail.com>
2026-01-31 14:10:03 +0100

    fix: standardize cn imports to @/lib/cn and remove @/lib/utils

--- web/src/components/ui/database-with-rest-api.tsx
+++ web/src/components/ui/database-with-rest-api.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import { motion } from "motion/react";
 import { Folder, HeartHandshakeIcon, SparklesIcon } from "lucide-react";
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/cn";
 
 interface DatabaseWithRestApiProps {
   className?: string;
